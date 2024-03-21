fd7d37d015f2e8f221144761c57814d0839b61cf
Johannes Schmidt <devshred@tigerflanke.de>
2024-03-21 21:40:54 +0100

    feat(data): handle power, cadence, heartrate and temperature in GPX-files

--- src/main/kotlin/org/devshred/gpstools/domain/proto/ProtobufTools.kt
+++ src/main/kotlin/org/devshred/gpstools/domain/proto/ProtobufTools.kt
@@ -6,7 +6,9 @@ import io.jenetics.jpx.TrackSegment
 import org.devshred.gpstools.domain.gps.PoiType
 import org.devshred.gpstools.domain.gps.WayPoint
 import org.devshred.gpstools.domain.gpx.GPX_CREATOR
+import org.w3c.dom.Node
 import java.time.Instant
+import javax.xml.parsers.DocumentBuilderFactory
 import io.jenetics.jpx.WayPoint as GpxWayPoint
 
 fun ProtoGpsContainer.toGpx(): GPX {
@@ -40,7 +42,51 @@ fun toProtoBuf(gpx: GpxWayPoint): ProtoWayPoint =
         if (gpx.time.isPresent) time = Timestamp.newBuilder().setSeconds(gpx.time.get().epochSecond).build()
         if (gpx.name.isPresent) name = gpx.name.get()
         if (gpx.symbol.isPresent) type = toProtoBuf(PoiType.fromGpxSym(gpx.symbol.get())!!)
+
+        gpx.extensions.map { document ->
+            val node: Node = document.documentElement
+            val extensionValues = exploreDocument(node)
+            extensionValues.power?.let { power = it }
+            extensionValues.cadence?.let { cadence = it }
+            extensionValues.temperature?.let { temperature = it }
+            extensionValues.heartRate?.let { heartrate = it }
+        }
     }
+
+fun exploreDocument(node: Node): ExtensionValues {
+    var extensionValues = ExtensionValues()
+    if (node.hasChildNodes()) {
+        var childNode = node.firstChild
+        while (childNode != null) {
+            when (childNode.nodeName) {
+                "power" -> extensionValues.power = childNode.textContent.toInt()
+                "gpxtpx:cad" -> extensionValues.cadence = childNode.textContent.toInt()
+                "gpxtpx:atemp" -> extensionValues.temperature = childNode.textContent.toInt()
+                "gpxtpx:hr" -> extensionValues.heartRate = childNode.textContent.toInt()
+            }
+            val newValues = exploreDocument(childNode)
+            extensionValues = extensionValues.union(newValues)
+            childNode = childNode.nextSibling
+        }
+    }
+    return extensionValues
+}
+
+data class ExtensionValues(
+    var power: Int? = null,
+    var cadence: Int? = null,
+    var temperature: Int? = null,
+    var heartRate: Int? = null,
+) {
+    fun union(newValues: ExtensionValues): ExtensionValues {
+        return this.copy(
+            power = newValues.power ?: this.power,
+            cadence = newValues.cadence ?: this.cadence,
+            temperature = newValues.temperature ?: this.temperature,
+            heartRate = newValues.heartRate ?: this.heartRate,
+        )
+    }
+}
 
 fun toProtoBuf(poiType: PoiType): ProtoPoiType =
     when (poiType) {
@@ -95,6 +141,53 @@ fun ProtoWayPoint.toGpx(): GpxWayPoint {
 
     if (hasType()) {
         builder.sym(PoiType.fromString(type.name).gpxSym)
+    }
+
+    if (hasSpeed()) {
+        builder.speed(speed)
+    }
+
+    if (hasPower() || hasCadence() || hasTemperature() || hasHeartrate()) {
+        val document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument()
+        val extensions = document.createElement("extensions")
+
+        if (hasPower()) {
+            val powerElement = document.createElement("power")
+            powerElement.textContent = power.toString()
+            extensions.appendChild(powerElement)
+        }
+
+        if (hasTemperature() || hasHeartrate() || hasCadence()) {
+            val trackPointExtensionElement = document.createElement("gpxtpx:TrackPointExtension")
+            val xmlnsAttribute = document.createAttribute("xmlns:gpxtpx")
+
+            // TODO: try to set xmlns once at root-level
+            xmlnsAttribute.value = "http://www.garmin.com/xmlschemas/TrackPointExtension/v1"
+            trackPointExtensionElement.setAttributeNodeNS(xmlnsAttribute)
+
+            if (hasTemperature()) {
+                val temperatureElement = document.createElement("gpxtpx:atemp")
+                temperatureElement.textContent = temperature.toString()
+                trackPointExtensionElement.appendChild(temperatureElement)
+            }
+
+            if (hasHeartrate()) {
+                val heartrateElement = document.createElement("gpxtpx:hr")
+                heartrateElement.textContent = heartrate.toString()
+                trackPointExtensionElement.appendChild(heartrateElement)
+            }
+
+            if (hasCadence()) {
+                val cadenceElement = document.createElement("gpxtpx:cad")
+                cadenceElement.textContent = cadence.toString()
+                trackPointExtensionElement.appendChild(cadenceElement)
+            }
+
+            extensions.appendChild(trackPointExtensionElement)
+        }
+
+        document.appendChild(extensions)
+        builder.extensions(document)
     }
 
     return builder.build()
